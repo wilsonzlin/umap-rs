0dc12bd8cb4b7af81f7aac329452809a6efe37e4
Wilson Lin <code@wilsonl.in>
2025-07-06 20:14:48 -0400

    Prune code

--- umap/umap_.py
+++ umap/umap_.py
@@ -1,11 +1,5 @@
-# Author: Leland McInnes <leland.mcinnes@gmail.com>
-#
-# License: BSD 3 clause
-from __future__ import print_function
-
 import locale
 from warnings import warn
-import time
 
 from scipy.optimize import curve_fit
 from sklearn.base import BaseEstimator, ClassNamePrefixFeaturesOutMixin
@@ -24,7 +18,6 @@ except ImportError:
 
 import numpy as np
 import scipy.sparse
-from scipy.sparse import tril as sparse_tril, triu as sparse_triu
 import scipy.sparse.csgraph
 import numba
 
@@ -35,8 +28,6 @@ import umap.sparse as sparse
 from umap.utils import (
     submatrix,
     ts,
-    csr_unique,
-    fast_knn_indices,
 )
 from umap.spectral import spectral_layout, tswspectral_layout
 from umap.layouts import (
@@ -44,10 +35,6 @@ from umap.layouts import (
     optimize_layout_generic,
     optimize_layout_inverse,
 )
-
-from pynndescent import NNDescent
-from pynndescent.distances import named_distances as pynn_named_distances
-from pynndescent.sparse import sparse_named_distances as pynn_sparse_named_distances
 
 locale.setlocale(locale.LC_NUMERIC, "C")
 
@@ -253,101 +240,6 @@ def smooth_knn_dist(distances, k, n_iter=64, local_connectivity=1.0, bandwidth=1
     return result, rho
 
 
-def nearest_neighbors(
-    X,
-    n_neighbors,
-    metric,
-    metric_kwds,
-    angular,
-    random_state,
-    low_memory=True,
-    use_pynndescent=True,
-    n_jobs=-1,
-    verbose=False,
-):
-    """Compute the ``n_neighbors`` nearest points for each data point in ``X``
-    under ``metric``. This may be exact, but more likely is approximated via
-    nearest neighbor descent.
-
-    Parameters
-    ----------
-    X: array of shape (n_samples, n_features)
-        The input data to compute the k-neighbor graph of.
-
-    n_neighbors: int
-        The number of nearest neighbors to compute for each sample in ``X``.
-
-    metric: string or callable
-        The metric to use for the computation.
-
-    metric_kwds: dict
-        Any arguments to pass to the metric computation function.
-
-    angular: bool
-        Whether to use angular rp trees in NN approximation.
-
-    random_state: np.random state
-        The random state to use for approximate NN computations.
-
-    low_memory: bool (optional, default True)
-        Whether to pursue lower memory NNdescent.
-
-    verbose: bool (optional, default False)
-        Whether to print status data during the computation.
-
-    Returns
-    -------
-    knn_indices: array of shape (n_samples, n_neighbors)
-        The indices on the ``n_neighbors`` closest points in the dataset.
-
-    knn_dists: array of shape (n_samples, n_neighbors)
-        The distances to the ``n_neighbors`` closest points in the dataset.
-
-    rp_forest: list of trees
-        The random projection forest used for searching (if used, None otherwise).
-    """
-    if verbose:
-        print(ts(), "Finding Nearest Neighbors")
-
-    if metric == "precomputed":
-        # Note that this does not support sparse distance matrices yet ...
-        # Compute indices of n nearest neighbors
-        knn_indices = fast_knn_indices(X, n_neighbors)
-        # knn_indices = np.argsort(X)[:, :n_neighbors]
-        # Compute the nearest neighbor distances
-        #   (equivalent to np.sort(X)[:,:n_neighbors])
-        knn_dists = X[np.arange(X.shape[0])[:, None], knn_indices].copy()
-        # Prune any nearest neighbours that are infinite distance apart.
-        disconnected_index = knn_dists == np.inf
-        knn_indices[disconnected_index] = -1
-
-        knn_search_index = None
-    else:
-        # TODO: Hacked values for now
-        n_trees = min(64, 5 + int(round((X.shape[0]) ** 0.5 / 20.0)))
-        n_iters = max(5, int(round(np.log2(X.shape[0]))))
-
-        knn_search_index = NNDescent(
-            X,
-            n_neighbors=n_neighbors,
-            metric=metric,
-            metric_kwds=metric_kwds,
-            random_state=random_state,
-            n_trees=n_trees,
-            n_iters=n_iters,
-            max_candidates=60,
-            low_memory=low_memory,
-            n_jobs=n_jobs,
-            verbose=verbose,
-            compressed=False,
-        )
-        knn_indices, knn_dists = knn_search_index.neighbor_graph
-
-    if verbose:
-        print(ts(), "Finished Nearest Neighbor Search")
-    return knn_indices, knn_dists, knn_search_index
-
-
 @numba.njit(
     locals={
         "knn_dists": numba.types.float32[:, ::1],
@@ -454,125 +346,6 @@ def fuzzy_simplicial_set(
     verbose=False,
     return_dists=None,
 ):
-    """Given a set of data X, a neighborhood size, and a measure of distance
-    compute the fuzzy simplicial set (here represented as a fuzzy graph in
-    the form of a sparse matrix) associated to the data. This is done by
-    locally approximating geodesic distance at each point, creating a fuzzy
-    simplicial set for each such point, and then combining all the local
-    fuzzy simplicial sets into a global one via a fuzzy union.
-
-    Parameters
-    ----------
-    X: array of shape (n_samples, n_features)
-        The data to be modelled as a fuzzy simplicial set.
-
-    n_neighbors: int
-        The number of neighbors to use to approximate geodesic distance.
-        Larger numbers induce more global estimates of the manifold that can
-        miss finer detail, while smaller values will focus on fine manifold
-        structure to the detriment of the larger picture.
-
-    random_state: numpy RandomState or equivalent
-        A state capable being used as a numpy random state.
-
-    metric: string or function (optional, default 'euclidean')
-        The metric to use to compute distances in high dimensional space.
-        If a string is passed it must match a valid predefined metric. If
-        a general metric is required a function that takes two 1d arrays and
-        returns a float can be provided. For performance purposes it is
-        required that this be a numba jit'd function. Valid string metrics
-        include:
-
-        * euclidean (or l2)
-        * manhattan (or l1)
-        * cityblock
-        * braycurtis
-        * canberra
-        * chebyshev
-        * correlation
-        * cosine
-        * dice
-        * hamming
-        * jaccard
-        * kulsinski
-        * ll_dirichlet
-        * mahalanobis
-        * matching
-        * minkowski
-        * rogerstanimoto
-        * russellrao
-        * seuclidean
-        * sokalmichener
-        * sokalsneath
-        * sqeuclidean
-        * yule
-        * wminkowski
-
-        Metrics that take arguments (such as minkowski, mahalanobis etc.)
-        can have arguments passed via the metric_kwds dictionary. At this
-        time care must be taken and dictionary elements must be ordered
-        appropriately; this will hopefully be fixed in the future.
-
-    metric_kwds: dict (optional, default {})
-        Arguments to pass on to the metric, such as the ``p`` value for
-        Minkowski distance.
-
-    knn_indices: array of shape (n_samples, n_neighbors) (optional)
-        If the k-nearest neighbors of each point has already been calculated
-        you can pass them in here to save computation time. This should be
-        an array with the indices of the k-nearest neighbors as a row for
-        each data point.
-
-    knn_dists: array of shape (n_samples, n_neighbors) (optional)
-        If the k-nearest neighbors of each point has already been calculated
-        you can pass them in here to save computation time. This should be
-        an array with the distances of the k-nearest neighbors as a row for
-        each data point.
-
-    angular: bool (optional, default False)
-        Whether to use angular/cosine distance for the random projection
-        forest for seeding NN-descent to determine approximate nearest
-        neighbors.
-
-    set_op_mix_ratio: float (optional, default 1.0)
-        Interpolate between (fuzzy) union and intersection as the set operation
-        used to combine local fuzzy simplicial sets to obtain a global fuzzy
-        simplicial sets. Both fuzzy set operations use the product t-norm.
-        The value of this parameter should be between 0.0 and 1.0; a value of
-        1.0 will use a pure fuzzy union, while 0.0 will use a pure fuzzy
-        intersection.
-
-    local_connectivity: int (optional, default 1)
-        The local connectivity required -- i.e. the number of nearest
-        neighbors that should be assumed to be connected at a local level.
-        The higher this value the more connected the manifold becomes
-        locally. In practice this should be not more than the local intrinsic
-        dimension of the manifold.
-
-    verbose: bool (optional, default False)
-        Whether to report information on the current progress of the algorithm.
-
-    return_dists: bool or None (optional, default None)
-        Whether to return the pairwise distance associated with each edge.
-
-    Returns
-    -------
-    fuzzy_simplicial_set: coo_matrix
-        A fuzzy simplicial set represented as a sparse matrix. The (i,
-        j) entry of the matrix represents the membership strength of the
-        1-simplex between the ith and jth sample points.
-    """
-    if knn_indices is None or knn_dists is None:
-        knn_indices, knn_dists, _ = nearest_neighbors(
-            X,
-            n_neighbors,
-            metric,
-            metric_kwds,
-            angular,
-            random_state,
-            verbose=verbose,
-        )
-
     knn_dists = knn_dists.astype(np.float32)
 
     sigmas, rhos = smooth_knn_dist(
@@ -1238,65 +1011,6 @@ def simplicial_set_embedding(
     if isinstance(embedding, list):
         aux_data["embedding_list"] = embedding
         embedding = embedding[-1].copy()
-
-    if output_dens:
-        if verbose:
-            print(ts() + " Computing embedding densities")
-
-        # Compute graph in embedding
-        (
-            knn_indices,
-            knn_dists,
-            rp_forest,
-        ) = nearest_neighbors(
-            embedding,
-            densmap_kwds["n_neighbors"],
-            "euclidean",
-            {},
-            False,
-            random_state,
-            verbose=verbose,
-        )
-
-        emb_graph, emb_sigmas, emb_rhos, emb_dists = fuzzy_simplicial_set(
-            embedding,
-            densmap_kwds["n_neighbors"],
-            random_state,
-            "euclidean",
-            {},
-            knn_indices,
-            knn_dists,
-            verbose=verbose,
-            return_dists=True,
-        )
-
-        emb_graph = emb_graph.tocoo()
-        emb_graph.sum_duplicates()
-        emb_graph.eliminate_zeros()
-
-        n_vertices = emb_graph.shape[1]
-
-        mu_sum = np.zeros(n_vertices, dtype=np.float32)
-        re = np.zeros(n_vertices, dtype=np.float32)
-
-        head = emb_graph.row
-        tail = emb_graph.col
-        for i in range(len(head)):
-            j = head[i]
-            k = tail[i]
-
-            D = emb_dists[j, k]
-            mu = emb_graph.data[i]
-
-            re[j] += mu * D
-            re[k] += mu * D
-            mu_sum[j] += mu
-            mu_sum[k] += mu
-
-        epsilon = 1e-8
-        re = np.log(epsilon + (re / mu_sum))
-
-        aux_data["rad_emb"] = re
 
     return embedding, aux_data
 
@@ -2118,10 +1832,6 @@ class UMAP(BaseEstimator, ClassNamePrefixFeaturesOutMixin):
 
         self._validate_parameters()
 
-        self._original_n_threads = numba.get_num_threads()
-        if self.n_jobs > 0 and self.n_jobs is not None:
-            numba.set_num_threads(self.n_jobs)
-
         # If we aren't asking for unique use the full index.
         # This will save special cases later.
         index = list(range(X.shape[0]))
@@ -2136,33 +1846,10 @@ class UMAP(BaseEstimator, ClassNamePrefixFeaturesOutMixin):
         # Standard case
         self._small_data = False
         # Standard case
-        if self._sparse_data and self.metric in pynn_sparse_named_distances:
-            nn_metric = self.metric
-        elif not self._sparse_data and self.metric in pynn_named_distances:
-            nn_metric = self.metric
-        else:
-            nn_metric = self._input_distance_func
-        if self.knn_dists is None:
-            (
-                self._knn_indices,
-                self._knn_dists,
-                self._knn_search_index,
-            ) = nearest_neighbors(
-                X[index],
-                self._n_neighbors,
-                nn_metric,
-                self._metric_kwds,
-                self.angular_rp_forest,
-                random_state,
-                self.low_memory,
-                use_pynndescent=True,
-                n_jobs=self.n_jobs,
-                verbose=self.verbose,
-            )
-        else:
-            self._knn_indices = self.knn_indices
-            self._knn_dists = self.knn_dists
-            self._knn_search_index = self.knn_search_index
+        nn_metric = self._input_distance_func
+        self._knn_indices = self.knn_indices
+        self._knn_dists = self.knn_dists
+        self._knn_search_index = self.knn_search_index
         # Disconnect any vertices farther apart than _disconnection_distance
         disconnected_index = self._knn_dists >= self._disconnection_distance
         self._knn_indices[disconnected_index] = -1
@@ -2245,7 +1932,6 @@ class UMAP(BaseEstimator, ClassNamePrefixFeaturesOutMixin):
             self.rad_orig_ = aux_data["rad_orig"][inverse]
             self.rad_emb_ = aux_data["rad_emb"][inverse]
 
-        numba.set_num_threads(self._original_n_threads)
         self._input_hash = joblib.hash(self._raw_data)
 
         # Set number of features out for sklearn API
